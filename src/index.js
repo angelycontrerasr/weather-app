49c239535644ae6beca6057daddebaa6ecb1f508
angelycontrerasr <angelycontrerasr@gmail.com>
2025-06-25 21:45:28 -0400

    caracas pre loaded info

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import "./styles.css";
 console.log('Happy developing ✨')
+let city = 'Caracas'
+fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' +city+'?key=SDRG7F4L9BSMN3GTENDM9ZX7Y', {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response);
+        document.getElementById('fullAddress').textContent=response.resolvedAddress
+        document.getElementById('description').textContent=response.description
+        document.getElementById('currentCondition').textContent = response.currentConditions.conditions
+    })
+;
+
+document.getElementById('city').textContent=city
